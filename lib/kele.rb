994204460a39518922e2c179de7b560c8eff517d
Jane Wang <jw9billions@gmail.com>
2017-09-02 20:28:43 -0700

    cp4

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,7 +13,12 @@ class Kele
 
   def get_me
     response = self.class.get(api_url('users/me'), headers: {"authorization" => @auth_token})
-    @user_info = JSON.parse(response.body) 
+    @user_data = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token})
+    @mentor_availability = JSON.parse(response.body)
   end
 
   private
