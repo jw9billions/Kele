f99dddba4df70d1caae1ba54cc2ed299fd476d54
Jane Wang <jw9billions@gmail.com>
2017-09-06 18:17:00 -0700

    cp5

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,13 @@
 require 'httparty'
 require 'json'
+#require 'roadmap'
+require '/Users/jane/BlocRuby/code/Kele/lib/roadmap.rb'
 
 class Kele
   include HTTParty
+  include Roadmap
   base_uri "https://www.bloc.io/api/v1/"
+
 
   def initialize(email, password)
     response = self.class.post(api_url("sessions"), body: {"email": email, "password": password})
---
+++ lib/roadmap.rb
@@ -0,0 +1,17 @@
+require 'HTTParty'
+
+module Roadmap
+  include HTTParty
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def get_roadmap(roadmap_id)
+    response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: {"authorization" => @auth_token})
+    @roadmap = JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get("/checkpoints/" + checkpoint_id.to_s, headers: {"authorization" => @auth_token})
+    @checkpoint = JSON.parse(response/body)
+  end
+
+end
